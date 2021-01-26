c99516d45fd946905622a8217d4b25ac9b3f41d5
Metty Schroeder <mettyj.schroeder@gmail.com>
2021-01-26 15:25:00 -0500

    fixed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
 const config = {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8080'
+  API_ENDPOINT: process.env.API_ENDPOINT || 'https://petful-metty.herokuapp.com' ||'http://localhost:8080'
 }
 
 export default config
